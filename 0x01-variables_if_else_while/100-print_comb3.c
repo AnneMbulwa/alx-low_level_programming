a2bd8c67efe22ad864f43968152237f10ce975bc
AnneMbulwa <annembulwa602@gmail.com>
2023-06-16 16:16:14 -0700

    prints combo c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,18 +5,18 @@
  */
 int main(void)
 {
-	int x = '0';
-	int y = '0';
+	int x;
+	int y;
 
-	for (x = '0'; x <= '8'; x++)
+	for (x = 48; x <= 56; x++)
 	{
-		for (y = '0'; y <= '9'; y++)
+		for (y = 49; y <= 57; y++)
 		{
 			if (y > x)
 			{
 				putchar(x);
 				putchar(y);
-				if (x != '8' && y != '9')
+				if (x != 56 && y != 57)
 				{
 					putchar(',');
 					putchar(' ');
