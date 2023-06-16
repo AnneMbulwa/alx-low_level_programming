7457c5f12f199deddedf908d356bee18495a73a4
AnneMbulwa <annembulwa602@gmail.com>
2023-06-16 15:58:01 -0700

    print combo c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main -  Program that prints all combinations of 2 digits
+ * Return: 0
+ */
+int main(void)
+{
+	int x = '0';
+	int y = '0';
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = '0'; y <= x; y++)
+		{
+			if (!(x == y) || (x > y))
+			{
+				putchar(x);
+				putchar(y);
+				if (x == '8' && y == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	return (0);
+}
