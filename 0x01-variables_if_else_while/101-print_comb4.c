3c7f719bd7ff9b04a48cbbe6520281bb8e026b16
AnneMbulwa <annembulwa602@gmail.com>
2023-06-16 16:38:04 -0700

    print combo4 c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main -  Program that prints all possible combinations of three digits
+ * Return: 0
+ */
+int main(void)
+{
+	int k = '0';
+	int b = '0';
+	int m = '0';
+
+	while (k <= '7')
+	{
+		while (b <= '8')
+		{
+			while (m <= '9')
+			{
+				if (k < b && j < k)
+				{
+					putchar(k);
+					putchar(b);
+					putchar(m);
+					if (!(k == '7' && b == '8' && m == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				m++;
+			}
+			m = '0';
+			b++;
+		}
+		b = '0';
+		k++;
+	}
+	putchar('\n');
+	return (0);
+}
+
