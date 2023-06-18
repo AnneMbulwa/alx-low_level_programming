41c5ed4e08c47afb55bb218fdc74859e479929a4
AnneMbulwa <annembulwa602@gmail.com>
2023-06-18 01:24:26 -0700

    print combo3 c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,16 +8,16 @@ int main(void)
 	int a;
 	int b;
 
-	for (a = '0'; a <= '9'; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (b = '1'; b <= '9'; b++)
+		for (b = 1; b <= 9; b++)
 		{
 
 			if (!(a > b) || a == b)
 			{
 				putchar(a);
 				putchar(b);
-				if (a == '8' && b == '9')
+				if (a == 8 && b == 9)
 				{
 					putchar('\n');
 				}
