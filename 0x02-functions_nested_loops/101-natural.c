e70383c96a20a616a6a3624bcd4b9085b9cd3d69
AnneMbulwa <annembulwa602@gmail.com>
2023-06-20 09:59:32 -0700

    natural c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,12 +5,11 @@
  */
 int main(void)
 {
-	int y;
-	int sum  = 0;
+	int y, sum = 0;
 
 	for (y = 0; y < 1024; y++)
 	{
-		if (y % 3 == 0 || y % 5 == 0)
+		if ((y % 3 == 0) || (y % 5) == 0)
 		{
 			sum + = y;
 		}
