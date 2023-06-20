ea49d968570891d252bec696bca0e3be15fa63af
AnneMbulwa <annembulwa602@gmail.com>
2023-06-20 08:11:35 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,8 +11,7 @@ void print_to_98(int n)
 	{
 		if (n == 98)
 		{
-			printf("%d", n);
-			printf("\n");
+			printf("%d\n", n);
 			break;
 		}
 		else
@@ -24,8 +23,7 @@ void print_to_98(int n)
 	{
 		if (n == 98)
 		{
-			printf("%d", n);
-			printf("%d", n);
+			printf("%d\n", n);
 			break;
 		}
 		else
