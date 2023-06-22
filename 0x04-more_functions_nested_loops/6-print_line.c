76f44c879f7314c8985fec5de9320432320c36d3
AnneMbulwa <annembulwa602@gmail.com>
2023-06-22 03:27:27 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,21 +2,22 @@
 /**
  *print_line -  draws a straight line in the terminal
  *@n: computed number
+ *Return: void
  */
 void print_line(int n)
 {
 	int a;
 
-	for (a = 0; a <= n; a++)
+	if (n <= 0)
 	{
-		if (n <= 0)
-		{
-			_putchar('\n');
-		}
-		else
-		{
-			_putchar(95);
-		}
 		_putchar('\n');
+	}
+	else
+	{
+	for (a = 0; a < n; a++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
 	}
 }
