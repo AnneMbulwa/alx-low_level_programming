2340d3ce27a04a4f32e75d8bf308e1985560a2b6
AnneMbulwa <annembulwa602@gmail.com>
2023-06-22 02:19:10 -0700

    prints numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_numbers -  from 0 to 9, followed by a new line.
+ *Return: 0
+ */
+void print_numbers(void)
+{
+	int y;
+
+	for (y = 0; y <= 9; y++)
+	{
+	_putchar(y + '0');
+	}
+	_putchar('\n');
+}
