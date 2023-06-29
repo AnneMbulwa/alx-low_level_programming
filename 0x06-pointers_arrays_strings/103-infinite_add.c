76478d945c2f4e7874939965999898780288a888
AnneMbulwa <annembulwa602@gmail.com>
2023-06-29 07:54:36 -0700

    print buffer

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *infinite_add -  function that adds two numbers.
+ *@n1: number one
+ *@n2: number two
+ *@r: buffer that the function will use to store the result
+ *size_r: buffer size
+ *Return: 0
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
