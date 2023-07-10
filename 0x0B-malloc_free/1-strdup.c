8bb59f20fdc3e57664114b9f969d6152f4b1e835
AnneMbulwa <annembulwa602@gmail.com>
2023-07-10 03:00:23 -0700

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,12 +11,14 @@ char *_strdup(char *str)
 	int len = 0;
 	int b = 0;
 
+	while (str[len] != '\0')
+		len++;
 	if (str == NULL)
 		return (NULL);
 	duplicate = malloc((len + 1) * sizeof(char));
 	if (duplicate == NULL)
 		return (NULL);
-	for (b = 0; str[b] != '\0'; b++)
+	for (b = 0; str[b]; b++)
 		duplicate[b] = str[b];
 	return (duplicate);
 }
