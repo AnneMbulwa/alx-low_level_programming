f6aceae36812f80b039b55fb43b6af140d62d3f3
AnneMbulwa <annembulwa602@gmail.com>
2023-07-10 06:28:11 -0700

    libmy.a

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -6,12 +6,5 @@
  */
 int _isalpha(int c)
 {
-if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
-{
-return (1);
-}
-else
-{
-return (0);
-}
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
