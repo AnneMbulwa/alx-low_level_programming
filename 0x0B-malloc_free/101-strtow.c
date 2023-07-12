93b24efaccbe27c09ab7822ea3d7055b90ccc7ff
AnneMbulwa <annembulwa602@gmail.com>
2023-07-12 02:37:17 -0700

    strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -33,18 +33,18 @@ int count_word(char *s)
  */
 char **strtow(char *str)
 {
-	char **words, *word_s;
+	char **array, *tmp;
 	int count = 0;
 	int len = 0;
-	int i, j, start, end, word;
+	int i, j = 0, start, end, words;
 
 	while (*(str + len))
 		len++;
-	word = count_word(str);
-	if (word == 0)
+	words = count_word(str);
+	if (words == 0)
 		return (NULL);
-	words = (char **)malloc(sizeof(char) * (word + 1));
-	if (words == NULL)
+	array = (char **)malloc(sizeof(char) * (words + 1));
+	if (array == NULL)
 		return (NULL);
 	for (i = 0; i <= len; i++)
 	{
@@ -54,21 +54,22 @@ char **strtow(char *str)
 			{
 				end = i;
 
-				word_s = malloc(sizeof(char) * (count + 1));
-				if (word_s == NULL)
+				tmp = (char *)malloc(sizeof(char) * (count + 1));
+				if (tmp == NULL)
 					return (NULL);
 				while (start < end)
-					*word_s++ = str[start++];
-			*word_s = '\0';
+					*tmp++ = str[start++];
+				*tmp = '\0';
 
-			words[j] = word_s - count;
-			j++;
+				array[j] = tmp - count;
+				j++;
+				count = 0;
 			}
 		}
 		else if (count++ == 0)
 			start = i;
 		}
-		words[j] = NULL;
+	array[j] = NULL;
 
-		return (words);
+	return (array);
 }
