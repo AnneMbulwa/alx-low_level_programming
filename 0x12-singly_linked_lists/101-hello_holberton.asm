72fac7f403a382c000477c2668005bf1ff5c8ba9
AnneMbulwa <annembulwa602@gmail.com>
2023-07-27 07:35:57 -0700

    hello holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,10 @@
-	global main
-	extern printf
+	global   main
+	  extern    printf
 main:
-	moe edi, format
-	xor ear,eax
-	call printf
-	mov eax,0
-	ret
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
 format: db `Hello, Holberton\n`,0
+
