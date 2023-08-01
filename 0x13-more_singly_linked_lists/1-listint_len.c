af3d55be383ceb09e9a076f35133e522a1d41d59
AnneMbulwa <annembulwa602@gmail.com>
2023-08-01 00:39:09 -0700

    listint_len

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ *listint_len - returns number of elements in listint_t list
+ *@h: pointer to listint_t list
+ *Return: number of elements in listint_t list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
