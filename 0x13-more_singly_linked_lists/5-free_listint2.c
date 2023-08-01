e9525467a1730b0c28381bd9cb8a7c50d6535471
AnneMbulwa <annembulwa602@gmail.com>
2023-08-01 01:28:03 -0700

    free listint2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,13 +9,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (*head == NULL)
+	if (*head == NULL)
+		return;
+	while (*head)
 	{
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 	}
-	if (*head == NULL)
-		return;
 	*head = NULL;
 }
