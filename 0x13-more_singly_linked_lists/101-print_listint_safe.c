d8e8c58ebde1680fac766dfe76d2473222b7fcec
AnneMbulwa <annembulwa602@gmail.com>
2023-08-01 07:09:00 -0700

    print listint safe

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,59 @@
+#include "lists.h"
+
+listint_t *find_listint_loop(listint_t *head);
+size_t print_listint_safe(const listint_t *head);
+
+/**
+ *find_listint_loop - finds the loop in listint_t
+ *@head: pointer to listint_t list
+ *Return: address of where loop start or NULL otherwise
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *first, *last;
+
+	if (head == NULL)
+		return (NULL);
+	last = head->next;
+	while (last != NULL)
+	{
+		if (last == last->next)
+			return (last);
+		first = head;
+		while (first != last)
+		{
+			if (first == last->next)
+				return (last->next);
+			first = first->next;
+		}
+		last = last->next;
+	}
+	return (NULL);
+}
+
+/**
+ *print_listint_safe - print listint_t list
+ *@head: pointer to listint_t list
+ *Return: number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t len = 0;
+	int index;
+	listint_t *ptr;
+
+	ptr = find_listint_loop((listint_t *) head);
+
+	while ((head != ptr || index) && head != NULL)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (head == ptr)
+			index = 0;
+		head = head->next;
+		len++;
+	}
+
+	if (ptr != NULL)
+		printf("-> [%p] %d\n", (void *)head, head->n);
+	return (len);
+}
