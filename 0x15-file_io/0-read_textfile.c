2d6b46ef02e5d1eb0c653e8fd47fc734cd5f31ab
AnneMbulwa <annembulwa602@gmail.com>
2023-08-07 23:39:25 -0700

    read textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,16 +9,16 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	char *b;
 	ssize_t n;
 	ssize_t z;
-	char *b;
 	ssize_t ptr;
 
 	ptr = open(filename, O_RDONLY);
 	if (ptr == -1)
 		return (0);
 
-	b = malloc(sizeof(char) * (letters));
+	b = malloc(sizeof(char) * letters);
 	n = read(ptr, b, letters);
 	z = write(STDERR_FILENO, b, n);
 
