16ee4e48bc886ac828dc0543bf70d4231fa9ad0d
AnneMbulwa <annembulwa602@gmail.com>
2023-08-11 06:25:04 -0700

    copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -55,7 +55,7 @@ int read_err(int fd1, int fd2, char *str)
 /**
  *create_buffer - creates buffer that allocates 1024 bytes
  *@ptr: name of file
- *Retuen: buffer created
+ *Return: buffer created
  */
 char *create_buffer(char *ptr)
 {
@@ -96,24 +96,25 @@ int main(int argc, char *argv[])
 	}
 	buf = create_buffer(argv[2]);
 	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY| O_TRUNC, 0664);
+	f = read(file_from, buf, 1024);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-	       if (file_from == -1)
-	       {
-		       free(buf);
+		if (file_from == -1)
+		{
+			free(buf);
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
-	       }
-	       n = write(file_to, buf, f);
-	       if (file_to == -1 || n == -1)
-	       {
-		       dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		       free(buf);
-		       exit(99);
-	       }
-	       f = read(file_from, buf, 1024);
-	       file_to = open(argv[2], O_WRONLY | O_APPEND);
+		}
+		n = write(file_to, buf, f);
+		if (file_to == -1 || n == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			free(buf);
+			exit(99);
+		}
+		f = read(file_from, buf, 1024);
+		file_to = open(argv[2], O_WRONLY | O_APPEND);
 	} while (f > 0);
 	free(buf);
 	close_file(file_from);
