1e4f84a50de30cf248d191a8c5f1165276247290
AnneMbulwa <annembulwa602@gmail.com>
2023-09-04 11:02:31 -0700

    file io

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -24,20 +24,20 @@ void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int m);
 
 /**
- *check_elf - checks if file is elf or not
+ *check_elf - Checks if file is elf or not
  *helps in distinguishing elf file from others
  *@e_ident: pointer to array containing the magic numbers
  */
 void check_elf(unsigned char *e_ident)
 {
-	int i = 0;
+	int a = 0;
 
-	for (i = 0; i < 4; i++)
+	for (a = 0; a < 4; a++)
 	{
-		if (e_ident[i] != 127 &&
-		    e_ident[i] != 'E' &&
-		    e_ident[i] != 'L' &&
-		    e_ident[i] != 'F')
+		if (e_ident[a] != 127 &&
+		    e_ident[a] != 'E' &&
+		    e_ident[a] != 'L' &&
+		    e_ident[a] != 'F')
 		{
 			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
 			exit(98);
@@ -46,21 +46,21 @@ void check_elf(unsigned char *e_ident)
 }
 
 /**
- *print_magic - prints magic numbers in the ELF headers files
+ *print_magic - Prints magic numbers in the ELF headers files
  *sets bytes to identify files as elf files
  *@e_ident: pointer to array that containg elf magic numbers
  */
 void print_magic(unsigned char *e_ident)
 {
-	int i = 0;
+	int a = 0;
 
-	printf("  Magic:   ");
+	printf(" Magic:	");
 
-	for (i = 0; i < EI_NIDENT; i++)
+	for (a = 0; a < EI_NIDENT; a++)
 	{
-		printf("%02x", e_ident[i]);
+		printf("%02x", e_ident[a]);
 
-		if (i == EI_NIDENT - 1)
+		if (a == EI_NIDENT - 1)
 			printf("\n");
 		else
 			printf(" ");
@@ -68,13 +68,13 @@ void print_magic(unsigned char *e_ident)
 }
 
 /**
- *print_class - specificies if file is 32-bit or 64-bit
+ *print_class - Specificies if elf file is 32-bit or 64-bit
  *@e_ident: pointer to array that contain ELF CLASS
  *
  */
 void print_class(unsigned char *e_ident)
 {
-	printf(" %-35s", "Class:");
+	printf("Class:		");
 	if (e_ident[EI_CLASS] == ELFCLASS64)
 	{
 		printf("ELF64\n");
@@ -91,13 +91,13 @@ void print_class(unsigned char *e_ident)
 /*little-endianess and big-endianess*/
 
 /**
- *print_data - determines the endianness of data in file
+ *print_data - Determines the endianness of data in file
  *@e_ident: pointer to array containing ELF DATA
  *
  */
 void print_data(unsigned char *e_ident)
 {
-	printf("Data:   ");
+	printf("Data:	");
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 	{
 		printf("2's complement, big endian\n");
@@ -111,12 +111,12 @@ void print_data(unsigned char *e_ident)
 }
 
 /**
- *print_version - indicates version of elf format used
+ *print_version - Indicates version of elf format used
  *@e_ident: pointer to array containing ELF CLASS
  */
 void print_version(unsigned char *e_ident)
 {
-	printf(" Version:        %d", e_ident[EI_VERSION]);
+	printf(" Version:	 %d", e_ident[EI_VERSION]);
 	if (e_ident[EI_VERSION] == EV_CURRENT)
 		printf("Current\n");
 	else
@@ -124,7 +124,7 @@ void print_version(unsigned char *e_ident)
 }
 
 /**
- *print_osabi - identifies the O.P and  ABI of ELF file
+ *print_osabi - Identifies the O.P and  ABI of ELF file
  *@e_ident: pointer to array containing ELF CLASS
  *Description: ABI (application binary interface)
  */
@@ -157,25 +157,26 @@ void print_osabi(unsigned char *e_ident)
 
 /**
  *print_abi - Specifies the ABI version
- *@e_ident: poniter to unsigned character of array containing ELF CLASS
+ *@e_ident: Pointer to unsigned character of array containing ELF ABI version
  */
 void print_abi(unsigned char *e_ident)
 {
-	printf(" ABI Version:		%d\n", e_ident[EI_ABIVERSION]);
+	printf("	ABI Version:		%d\n",
+			e_ident[EI_ABIVERSION]);
 }
 
 /**
- *print_type - Specify type of ELF file
- *@e_type: unsigned elf type
- *@e_ident: pointer to unsigned character of array containg ELF CLASS
- *Description: types of elf files are executable, shared or relocatable
+ * print_type - Specify type of ELF file
+ * @e_type: unsigned elf type
+ * @e_ident: pointer to unsigned character of array containg ELF CLASS
+ * Description: types of elf files are executable, shared or relocatable
  *
  */
 void print_type(unsigned int e_type, unsigned char *e_ident)
 {
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 		e_type >>= 8;
-	printf(" Type:	");
+	printf(" Type:			");
 
 	if (e_type == ET_NONE)
 		printf("NONE\n");
@@ -193,8 +194,8 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
 
 /**
  *print_entry - Indicates memory address where program start
- *@e_ident: pointer to unsigned charcacter of array containing ELF CLASS
  *@e_entry: address of unsigned  ELF entry point
+ *@e_ident: pointer to unsigned charcacter of array containing ELF CLASS
  *Description: entry point access
  */
 void print_entry(unsigned long int e_entry, unsigned char *e_ident)
@@ -209,10 +210,10 @@ void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 	}
 
 	if (e_ident[EI_CLASS] == ELFCLASS32)
-		printf("%x\n", (unsigned int)e_entry);
+		printf("%#x\n", (unsigned int)e_entry);
 
 	else
-		printf("%lx\n", e_entry);
+		printf("%#lx\n", e_entry);
 }
 
 /**
@@ -266,11 +267,11 @@ int main(int argc, char *argv[])
 		free(ptr);
 		close_elf(y);
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
-		return (98);
+		exit(98);
 	}
 
 	check_elf(ptr->e_ident);
-	printf("ELF Header:\n Magic:	");
+	printf("ELF Header:\n ");
 	print_magic(ptr->e_ident);
 	print_class(ptr->e_ident);
 	print_data(ptr->e_ident);
