4c7e022350362d4ab40275b7852117ae22f7c99b
AnneMbulwa <annembulwa602@gmail.com>
2023-10-12 08:19:40 -0700

    doubly lined list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,10 +8,6 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	int counter = 0;
 
-	if (h == NULL)
-		return (counter);
-	while (h->prev != NULL)
-		h = h->prev;
 	while (h != NULL)
 	{
 		printf("%d\n" h->n);
