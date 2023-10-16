3f2cc50e26494695647b1bb265c5a2f6cc1fb222
AnneMbulwa <annembulwa602@gmail.com>
2023-10-16 05:21:57 -0700

    code injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p /tmp https://github.com/AnneMbulwa/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libtestings.so
+wget -p /tmp https://github.com/AnneMbulwa/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libtestings.so1
 export LD_PRELOAD=/tmp/libtestings.so1
