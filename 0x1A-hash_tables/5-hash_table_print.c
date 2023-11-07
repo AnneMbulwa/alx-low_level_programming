4342d3259836dde55d4110f4b184325d21e11e53
AnneMbulwa <annembulwa602@gmail.com>
2023-11-07 17:32:55 +0300

    hash tables

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_print - function that prints a hash table.
+ *@ht: hash table
+ *Description: print the key/value in the order that they appear
+ *in the array of hash table: Order: array, list
+ *Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int a;
+	hash_node_t *node;
+
+	if (ht == NULL)
+		return (NULL);
+
+}
