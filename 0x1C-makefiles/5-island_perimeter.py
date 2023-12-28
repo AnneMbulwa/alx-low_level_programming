3b5a199dcf784800c973519b5c94807849904287
AnneMbulwa <annembulwa602@gmail.com>
2023-12-28 19:41:57 +0300

    makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python3
 def island_perimeter(grid):
+    """
+    returns the perimeter of the island described in grid
+    Args:
+        grid[0]: water
+        grid[1]: land
+
+    One cell is a square with side length 1
+    Grid cells are connected horizontally/vertically (not diagonally)
+    Grid is rectangular, width and height don’t exceed 100
+    """
+    perimeter = 0
+    s = 0
+
+    for a in range(len(grid)):
+        for b in range(len(grid[0])):
+            if grid[a][b] == 1:
+                s += 1
+                if (b > 0 and grid[a][b - 1] == 1):
+                    perimeter += 1
+                if (a > 0 and grid[a - 1][b] == 1):
+                    perimeter += 1
+    return s * 4 - perimeter * 2
