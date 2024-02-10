0d11232fd4f359e7966cedf239c5e0b0379125ae
AnneMbulwa <annembulwa602@gmail.com>
2024-02-10 23:42:34 +0300

    linear search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -44,6 +44,10 @@ typedef struct skiplist_s
 } skiplist_t;
 skiplist_t *linear_skip(skiplist_t *list, int value);
 
+skiplist_t *create_skiplist(int *array, size_t size);
+void print_skiplist(const skiplist_t *list);
+void free_skiplist(skiplist_t *list);
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
